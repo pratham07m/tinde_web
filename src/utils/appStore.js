a6778607de75f7be4c19c4bb5f4c10b8da13ebc8
Pratham_Mahant <pratham2004m@gmail.com>
2025-01-26 22:08:05 +0530

    create a connection req page

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice"
+import connectionReducer from "./conectionSlice"
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
-    feed:feedReducer
+    feed:feedReducer,
+    connections:connectionReducer,
   },
-  
 });
 
 export default appStore;
