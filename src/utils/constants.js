63dcfa7bb207c227b5df1ca8e6216f9510365966
Pratham_Mahant <pratham2004m@gmail.com>
2025-02-07 12:45:09 +0530

    chnges

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "/api"
+// production
+// export const BASE_URL = "/api"
+
+//dev mode 
+export const BASE_URL = location.hostname == "localhost" ? "http://localhost:7777" : "/api"
+
